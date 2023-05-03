6ecda8334f3afd03a773112e8ba5e9bbe67271b1
Labraderp <kalithesharpclaw@gmail.com>
2023-05-03 14:51:21 -0400

    USERPROPG: user profile page completed

---
+++ beerbuddies-frontend/src/pages/UserProfilePage.jsx
@@ -0,0 +1,26 @@
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container"
+import creedpic from '../images/creed.jpeg';
+
+
+export default function UserProfilePage() {
+    return(
+        <div>
+            <Container>
+                <Row>
+                    <Col>
+                        <Row><img src={creedpic} /></Row>
+                        <Row>@myuserhandle</Row>
+                        <Row>City, State</Row>
+                        <Row>Beer Preferences</Row>
+                    </Col>
+                    <Col>
+                        <Row>Token Count</Row>
+                        <Row>Recent Reviews</Row>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
