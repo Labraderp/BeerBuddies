82a0e3e9359d6e3efaa02d6d8358fbb9871b13a9
Nick L <lukeynic000@gmail.com>
2023-05-04 13:00:47 -0400

    updated the user model

--- beerbuddies_project/beerbuddies_app/models.py
+++ beerbuddies_project/beerbuddies_app/models.py
@@ -9,6 +9,30 @@ class App_User(AbstractUser):
         max_length=255, null=False, blank=False, unique=True)
     USERNAME_FIELD = "handle"
     REQUIRED_FIELDS = []
+    profile_info = models.TextField(max_length=1000, default='')
+    token_amount = models.IntegerField(blank=True, default=0)
 
     def __str__(self):
         return f"{self.handle} | {self.email}"
+
+
+# class Beer_Garden(models.Model):
+#     user = models.ForeignKey(
+#         App_User, on_delete=models.CASCADE, related_name='user')
+#     # beers = models.ManyToManyField(Beer, related_name="beer_gardens")
+
+#     def __str__(self):
+#         return f"{self.user.handle}'s beer garden'"
+
+#     # def __str__(self):
+#     #     return f"{self.beer} ({self.user.handle})"
+
+
+# class Beer(models.Model):
+#     name = models.CharField(max_length=255)
+#     description = models.TextField(max_length=1000)
+#     beer_garden = models.ManyToManyField(
+#         Beer_Garden, related_name="beer_gardens")
+
+#     def __str__(self):
+#         return f"{self.name} | {self.description}"
