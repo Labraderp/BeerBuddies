77f211ffd7b66c9273d8302241259f8b06549f4f
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-05-08 12:59:35 -0600

    save

--- beerbuddies-frontend/src/pages/ErrorPage.jsx
+++ beerbuddies-frontend/src/pages/ErrorPage.jsx
@@ -1,8 +1,10 @@
+import beerBottle from '../images/beerBottleSmash.gif'
+
 export default function ErrorPage() {
     return (
         <div className="error">
             <h1>404 not found</h1>
-            <img src="" alt="" />
+            <img src={beerBottle} alt="" />
         </div>
     )
 }
