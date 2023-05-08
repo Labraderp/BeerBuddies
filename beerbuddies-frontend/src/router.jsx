6454b5cb1d7f2d72464e498736e7bc84d8c70d60
Johnny <j8ventura@gmail.com>
2023-05-08 11:28:06 -0500

    updated links in homepage

--- beerbuddies-frontend/src/router.jsx
+++ beerbuddies-frontend/src/router.jsx
@@ -1,39 +1,62 @@
-import { createBrowserRouter, createHashRouter } from "react-router-dom";
+// import { createBrowserRouter, createHashRouter } from "react-router-dom";
+// import App from "./App";
+// import ErrorPage from "./pages/ErrorPage";
+// import Home from "./components/Home";
+// import BuddiesPage from "./pages/BuddiesPage"
+// import UserProfilePage from "./pages/UserProfilePage"
+// import BeerGarden from "./pages/BeerGarden"
+
+// const router = createHashRouter ([
+//     {
+//         path: "/",
+//         element: <App />,
+//         errorElement: <ErrorPage />,
+//         children: [
+//             {   
+//                 index: true,
+//                 element: <App />
+//             }, 
+//             {
+//                 path: "login",
+//                 element: <Home />
+//             },
+//             {
+//                 path: "/BuddiesPage",
+//                 element: <BuddiesPage />
+//             },
+//             {
+//                 path: "/BeerGarden",
+//                 element: <BeerGarden />
+//             },
+//             {
+//                 path: "/UserProfilePage",
+//                 element: <UserProfilePage />
+//             }
+//         ]
+//     }
+// ])
+
+// export default router;
+
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
 import ErrorPage from "./pages/ErrorPage";
 import Home from "./components/Home";
-import BuddiesPage from "./pages/BuddiesPage"
-import UserProfilePage from "./pages/UserProfilePage"
-import BeerGarden from "./pages/BeerGarden"
+import BuddiesPage from "./pages/BuddiesPage";
+import UserProfilePage from "./pages/UserProfilePage";
+import BeerGarden from "./pages/BeerGarden";
 
-const router = createHashRouter ([
-    {
-        path: "/",
-        element: <App />,
-        errorElement: <ErrorPage />,
-        children: [
-            {   
-                index: true,
-                element: <App />
-            }, 
-            {
-                path: "login",
-                element: <Home />
-            },
-            {
-                path: "/BuddiesPage",
-                element: <BuddiesPage />
-            },
-            {
-                path: "/BeerGarden",
-                element: <BeerGarden />
-            },
-            {
-                path: "/UserProfilePage",
-                element: <UserProfilePage />
-            }
-        ]
-    }
-])
+const router = (
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Home />} />
+      <Route path="/BuddiesPage" element={<BuddiesPage />} />
+      <Route path="/BeerGarden" element={<BeerGarden />} />
+      <Route path="/UserProfilePage" element={<UserProfilePage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  </Router>
+);
 
 export default router;
