90efdfc7b824dd3627756ea2d8c84128a477915c
Johnny <j8ventura@gmail.com>
2023-05-16 13:24:20 -0500

    buying beer decrements tokens

--- beerbuddies-frontend/src/pages/BeerPage.jsx
+++ beerbuddies-frontend/src/pages/BeerPage.jsx
@@ -3,8 +3,38 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import creedpic from '../images/creed.jpeg'
+import React, { useContext } from 'react';
+import { userContext } from '../App';
+import { decrementToken } from '../utilities';
 
 export default function BeerPage() {
+    const user_info = useContext(userContext)
+
+    const handleBuyBeer = async () => {
+        try {
+            // Check if the user has enough tokens
+            if (user_info.user.token_amount > 0) {
+                const result = await decrementToken(user_info.user.handle);
+                if (result) {
+                    const updatedUserContext = {
+                        ...user_info.user,
+                        token_amount: user_info.user.token_amount - 1,
+                    };
+                    user_info.setUser(updatedUserContext);
+                    // Show a success message
+                    alert('1 token used');
+                } else {
+                    console.error('Failed to decrement token amount.');
+                }
+            } else {
+                // Show a failure message
+                alert('You do not have enough tokens. Please purchase more.');
+            }
+        } catch (error) {
+            console.error('Error:', error.message);
+        }
+    };
+
     return(
         <div>
             <Container>
@@ -18,7 +48,7 @@ export default function BeerPage() {
                     <Col>
                         <Row>Beer Name</Row>
                         <Row>Notes/Flavors</Row>
-                        <Row><Button variant='success'>Buy Beer</Button></Row>
+                        <Row><Button variant='success' onClick={handleBuyBeer}>Buy Beer</Button></Row>
                     </Col>
                 </Row>
             </Container>
